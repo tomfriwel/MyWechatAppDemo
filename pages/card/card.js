21757b1abf04f3912be1b9eef05ad5a00782c86f
tomfriwel <976932447@qq.com>
2018-06-27 09:25:51 +0800

    card test

--- pages/card/card.js
+++ pages/card/card.js
@@ -1,78 +1,36 @@
 // card.js
+
+const api = require('../../utils/api.js')
+const net = require('../../utils/network.js')
+
 Page({
+    data: {
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    wx.addCard({
-      cardList: [
-        {
-          cardId: 'pgII0vzkWtwU0GxFbfCkHOFkR8lg'
-        }
-      ],
-      success: function (res) {
-        console.log(res.cardList) // 卡券添加结果
-      },
-      fail: function(res) {
-        console.log(res);
-      }
-    })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+    },
+    onLoad: function(options) {
+    },
+    onShow: function(options) {
+        console.log(options)
+    },
+    addCard: function() {
+        net.post({
+            url: api.wechat.getAddCardConfig,
+            data:{},
+            success: function(res) {
+                console.log(res)
+                let obj = res.data
+                
+                wx.addCard({
+                    cardList: [obj], // 需要添加的卡券列表
+                    success: function (res) {
+                        // var cardList = res.cardList; // 添加的卡券列表信息
+                        console.log(res)
+                    },
+                    fail:function(res){
+                        console.log(res)
+                    }
+                });
+            }
+        })
+    }
 })
--- utils/api.js
+++ utils/api.js
@@ -6,22 +6,25 @@ function makeUrl(apiUrl) {
     return serverUrl + apiUrl
 }
 
+function makeUrlMap(map) {
+    let obj = {}
+    for (let key in map) {
+        obj[key] = makeUrl(map[key])
+    }
+    return obj
+}
+
 const wechat = {
     getUserInfo: '/wechat/getUserInfo',
     login:'/wechat/login',
     getDecodeEncryptedData:'/wechat/getDecodeEncryptedData',
     bindGroupAndUser: '/wechat/bindGroupAndUser',
-    addUser:'/wechat/addUser',
-    getGroupUserList: '/wechat/getGroupUserList'
+    addUser: '/wechat/addUser',
+    getGroupUserList: '/wechat/getGroupUserList',
+    getAddCardConfig: '/wechatTest/getAddCardConfig'
 }
 
 module.exports = {
     serverUrl: serverUrl,
-    wechat: {
-        login: makeUrl(wechat.login),
-        getDecodeEncryptedData: makeUrl(wechat.getDecodeEncryptedData),
-        bindGroupAndUser: makeUrl(wechat.bindGroupAndUser),
-        addUser: makeUrl(wechat.addUser),
-        getGroupUserList: makeUrl(wechat.getGroupUserList)
-    }
+    wechat: makeUrlMap(wechat)
 }
