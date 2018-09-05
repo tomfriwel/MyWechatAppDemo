75313dfcf1fb5cbe044ffd9097dbabb231a17577
tomfriwel <976932447@qq.com>
2018-09-05 14:43:36 +0800

    font

--- pages/detectfont/detectfont.js
+++ pages/detectfont/detectfont.js
@@ -1,66 +1,45 @@
 // pages/detectfont/detectfont.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+    },
+    loadFont() {
+        wx.loadFontFace({
+            family: 'Catamaran SemiBold',
+            source: 'url("https://www.tomfriwel.com/static/font/Catamaran-SemiBold.ttf")',
+            success: function (res) {
+                console.log(res.status) //  loaded
+            },
+            fail: function (res) {
+                console.log(res.status) //  error
+            },
+            complete: function (res) {
+                console.log(res.status);
+            }
+        });
+        wx.loadFontFace({
+            family: 'AritaSans Bold',
+            source: 'url("https://www.tomfriwel.com/static/font/AritaSans-Bold.ttf")',
+            success: function (res) {
+                console.log(res.status) //  loaded
+            },
+            fail: function (res) {
+                console.log(res) //  error
+            },
+            // complete: function (res) {
+            //     console.log(res.status);
+            // }
+        });
+    }
 })
