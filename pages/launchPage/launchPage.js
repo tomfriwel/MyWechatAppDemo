416a9cb70dac39442967de48f6a08017d2ccc000
tomfriwel <976932447@qq.com>
2018-09-05 14:50:20 +0800

    m

--- pages/launchPage/launchPage.js
+++ pages/launchPage/launchPage.js
@@ -4,23 +4,12 @@ const app = getApp()
 const network = require('../../utils/network.js')
 
 Page({
-    /**
-     * 页面的初始数据
-     */
     data: {
 
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
     onLoad: function (options) {
         console.log('launchPage onLoad:' + (new Date()).getTime())
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
     onReady: function () {
         console.log('launchPage onReady:' + (new Date()).getTime())
         app.loginCallback = function () {
@@ -39,25 +28,7 @@ Page({
             }, 500)
         })
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
     onShow: function () {
         console.log('launchPage onShow:' + (new Date()).getTime())
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
     },
 })
