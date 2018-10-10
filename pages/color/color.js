97f5b6ab6a7fc5138a510f8f938d976f85b714bf
tomfriwel <976932447@qq.com>
2018-10-10 17:41:04 +0800

    color

---
+++ pages/color/color.js
@@ -0,0 +1,23 @@
+// pages/color/color.js
+Page({
+    data: {
+        colors: [{
+                r: 220,
+                g: 20,
+                b: 20
+            },
+            {
+                r: 120,
+                g: 120,
+                b: 120
+            }, {
+                r: 170,
+                g: 70,
+                b: 70
+            },
+        ]
+    },
+    onLoad: function(options) {
+
+    },
+})
