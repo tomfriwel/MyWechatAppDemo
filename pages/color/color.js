84128649d03c1e30dd9a780ebd27eeef37136614
tomfriwel <976932447@qq.com>
2018-10-11 16:03:28 +0800

    picker

--- pages/color/color.js
+++ pages/color/color.js
@@ -1,5 +1,5 @@
 // pages/color/color.js
-
+const tools = require('./color-conversion-algorithms.js')
 const W = wx.getSystemInfoSync().screenWidth
 const rate = 750.0 / W
 const HUE_H = 600 //rpx
@@ -8,6 +8,16 @@ console.log(rate)
 
 function inRange(value, min, max) {
     return value >= min && value < max
+}
+
+function sb2sl(s, b) {
+    let SL = {
+        s: 0,
+        l: 0
+    }
+    SL.l = (2 - s) * b / 2;
+    SL.s = SL.l && SL.l < 1 ? s * b / (SL.l < 0.5 ? SL.l * 2 : 2 - SL.l * 2) : SL.s;
+    return SL
 }
 Page({
     data: {
@@ -62,23 +72,98 @@ Page({
             g: 0,
             b: 0
         },
+        hue: 1,
+        pickerColor: {
+            r: 0,
+            g: 0,
+            b: 0
+        },
         huePosition: 0
     },
     onLoad: function(options) {
 
     },
-    hueHandler(e) {
-        console.log(e)
-        let {
-            x,
-            y
-        } = e.detail
-        let {
-            offsetTop
-        } = e.currentTarget
+    // color picker
+    // colorPickerHandler(e) {
+    //     console.log(e)
+    //     let {
+    //         x,
+    //         y
+    //     } = e.detail
+    //     let {
+    //         offsetTop
+    //     } = e.currentTarget
 
-        console.log(y - offsetTop)
+    //     console.log(y - offsetTop)
+    // },
+    handleColorPickerTouch(e) {
+        if (e.touches.length == 1) {
+            let {
+                pageX,
+                pageY,
+            } = e.touches[0]
+            let {
+                offsetLeft,
+                offsetTop,
+            } = e.currentTarget
+
+            let posX = pageX - offsetLeft
+            let posY = pageY - offsetTop
+            posX = posX > MAX_HUE ? MAX_HUE : (posX < 0 ? 0 : posX)
+            posX *= rate
+            let percentX = posX / HUE_H
+
+            posY = posY > MAX_HUE ? MAX_HUE : (posY < 0 ? 0 : posY)
+            posY *= rate
+            let percentY = posY / HUE_H
+
+            // console.log(percentX + ',' + percentY)
+
+            // to right saturation
+            // to top lightness
+            let temp = tools.hslToRgb({
+                h: this.data.hue,
+                s: percentX,
+                l: sb2sl(percentX, 1 - percentY).l
+            })
+            let pickerColor = {
+                r: parseInt(temp[0]),
+                g: parseInt(temp[1]),
+                b: parseInt(temp[2])
+            }
+            this.setData({
+                colorPickerPosition: {
+                    x: posX,
+                    y: posY,
+                },
+                pickerColor
+            })
+            console.log(pickerColor)
+            console.log({
+                h: this.data.hue,
+                s: percentX,
+                l: sb2sl(percentX, 1 - percentY).l
+            })
+        }
     },
+    colorPickerStart(e) {
+        this.handleColorPickerTouch(e)
+    },
+    colorPickerMove(e) {
+        this.handleColorPickerTouch(e)
+    },
+    // hue
+    // hueHandler(e) {
+    //     let {
+    //         x,
+    //         y
+    //     } = e.detail
+    //     let {
+    //         offsetTop
+    //     } = e.currentTarget
+
+    //     console.log(y - offsetTop)
+    // },
     handleHueTouch(e) {
         if (e.touches.length == 1) {
             let {
@@ -91,7 +176,7 @@ Page({
             let pos = pageY - offsetTop
             pos = pos > MAX_HUE ? MAX_HUE : (pos < 0 ? 0 : pos)
             pos *= rate
-            // console.log(pos)
+
             let percent = pos / HUE_H
             // console.log(percent + '%')
             // 1530
@@ -101,73 +186,21 @@ Page({
             // 50% ~ 67% (0,255,255) ~ (0,0,255)
             // 67% ~ 83% (0,0,255) ~ (255,0,255)
             // 83% ~ 100% (255,0,255) ~ (255,0,0)
-            let v = 1530 * percent
-            let isZero = v == 0
-            let n = parseInt(v / 255)
-            v = v - n * 255
-            if (v == 0 && !isZero) {
-                v = 255
-            }
-            v = parseInt(v)
-            console.log(v + ',' + percent)
-            percent *= 100
+
+            let temp = tools.hslToRgb({
+                h: percent,
+                s: 1,
+                l: 0.5
+            })
             let color = {
-                r: 0,
-                g: 0,
-                b: 0
+                r: parseInt(temp[0]),
+                g: parseInt(temp[1]),
+                b: parseInt(temp[2])
             }
-            if (inRange(percent, 0, 17)) {
-                color = {
-                    r: 255,
-                    g: v,
-                    b: 0
-                }
-                console.log(1)
-            } else if (inRange(percent, 17, 33)) {
-                color = {
-                    r: 255 - v,
-                    g: 255,
-                    b: 0
-                }
-
-                console.log(2)
-            } else if (inRange(percent, 33, 50)) {
-                color = {
-                    r: 0,
-                    g: 255,
-                    b: v
-                }
-            } else if (inRange(percent, 50, 67)) {
-                color = {
-                    r: 0,
-                    g: 255 - v,
-                    b: 255
-                }
-            } else if (inRange(percent, 67, 83)) {
-                color = {
-                    r: v,
-                    g: 0,
-                    b: 255
-                }
-            } else if (inRange(percent, 83, 100)) {
-                color = {
-                    r: 255,
-                    g: 0,
-                    b: 255 - v
-                }
-            } else {
-                color = {
-                    r: 255,
-                    g: 0,
-                    b: 0
-                }
-                console.log(3)
-            }
-
-            // (to bottom, #ff0000 0%, #ffff00 17%, #00ff00 33%, #00ffff 50%, #0000ff 67%, #ff00ff 83%, #ff0000 100%);
             this.setData({
                 huePosition: pos,
-                color
+                hue: percent,
+                color: color
             })
         }
     },
