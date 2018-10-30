8321f7ec2afb94cc29165dcec65e1e2622e36ec9
tomfriwel <976932447@qq.com>
2018-10-30 13:51:12 +0800

    清音

--- pages/japanese50/japanese50.js
+++ pages/japanese50/japanese50.js
@@ -74,6 +74,301 @@ const jp = {
                 subindex: 4,
             },
         ],
+        [{
+                hiragana: 'さ',
+                katakana: 'サ',
+                en: 'sa',
+                index: 2,
+                subindex: 0,
+            },
+            {
+                hiragana: 'し',
+                katakana: 'シ',
+                en: 'shi',
+                index: 2,
+                subindex: 1,
+            },
+            {
+                hiragana: 'す',
+                katakana: 'ス',
+                en: 'su',
+                index: 2,
+                subindex: 2,
+            },
+            {
+                hiragana: 'せ',
+                katakana: 'セ',
+                en: 'se',
+                index: 2,
+                subindex: 3,
+            },
+            {
+                hiragana: 'そ',
+                katakana: 'ソ',
+                en: 'so',
+                index: 2,
+                subindex: 4,
+            },
+        ],
+        [{
+                hiragana: 'た',
+                katakana: 'タ',
+                en: 'ta',
+                index: 3,
+                subindex: 0,
+            },
+            {
+                hiragana: 'ち',
+                katakana: 'チ',
+                en: 'chi',
+                index: 3,
+                subindex: 1,
+            },
+            {
+                hiragana: 'つ',
+                katakana: 'ツ',
+                en: 'tsu',
+                index: 3,
+                subindex: 2,
+            },
+            {
+                hiragana: 'て',
+                katakana: 'テ',
+                en: 'te',
+                index: 3,
+                subindex: 3,
+            },
+            {
+                hiragana: 'と',
+                katakana: 'ト',
+                en: 'to',
+                index: 3,
+                subindex: 4,
+            },
+        ],
+        [{
+                hiragana: 'な',
+                katakana: 'ナ',
+                en: 'na',
+                index: 4,
+                subindex: 0,
+            },
+            {
+                hiragana: 'に',
+                katakana: 'ニ',
+                en: 'ni',
+                index: 4,
+                subindex: 1,
+            },
+            {
+                hiragana: 'ぬ',
+                katakana: 'ヌ',
+                en: 'nu',
+                index: 4,
+                subindex: 2,
+            },
+            {
+                hiragana: 'ね',
+                katakana: 'ネ',
+                en: 'ne',
+                index: 4,
+                subindex: 3,
+            },
+            {
+                hiragana: 'の',
+                katakana: 'ノ',
+                en: 'no',
+                index: 4,
+                subindex: 4,
+            },
+        ],
+        [{
+                hiragana: 'は',
+                katakana: 'ハ',
+                en: 'ha',
+                index: 5,
+                subindex: 0,
+            },
+            {
+                hiragana: 'ひ',
+                katakana: 'ヒ',
+                en: 'hi',
+                index: 5,
+                subindex: 1,
+            },
+            {
+                hiragana: 'ふ',
+                katakana: 'フ',
+                en: 'fu',
+                index: 5,
+                subindex: 2,
+            },
+            {
+                hiragana: 'へ',
+                katakana: 'ヘ',
+                en: 'he',
+                index: 5,
+                subindex: 3,
+            },
+            {
+                hiragana: 'ほ',
+                katakana: 'ホ',
+                en: 'ho',
+                index: 5,
+                subindex: 4,
+            },
+        ],
+        [{
+                hiragana: 'ま',
+                katakana: 'マ',
+                en: 'ma',
+                index: 6,
+                subindex: 0,
+            },
+            {
+                hiragana: 'み',
+                katakana: 'ミ',
+                en: 'mi',
+                index: 6,
+                subindex: 1,
+            },
+            {
+                hiragana: 'む',
+                katakana: 'ム',
+                en: 'mu',
+                index: 6,
+                subindex: 2,
+            },
+            {
+                hiragana: 'め',
+                katakana: 'メ',
+                en: 'me',
+                index: 6,
+                subindex: 3,
+            },
+            {
+                hiragana: 'も',
+                katakana: 'モ',
+                en: 'mo',
+                index: 6,
+                subindex: 4,
+            },
+        ],
+        [{
+                hiragana: 'や',
+                katakana: 'ヤ',
+                en: 'ya',
+                index: 7,
+                subindex: 0,
+            },
+            {
+                hiragana: 'い',
+                katakana: 'イ',
+                en: 'i',
+                index: 7,
+                subindex: 1,
+            },
+            {
+                hiragana: 'ゆ',
+                katakana: 'ユ',
+                en: 'yu',
+                index: 7,
+                subindex: 2,
+            },
+            {
+                hiragana: 'え',
+                katakana: 'エ',
+                en: 'e',
+                index: 7,
+                subindex: 3,
+            },
+            {
+                hiragana: 'よ',
+                katakana: 'ヨ',
+                en: 'yo',
+                index: 7,
+                subindex: 4,
+            },
+        ],
+        [{
+                hiragana: 'ら',
+                katakana: 'ラ',
+                en: 'ra',
+                index: 8,
+                subindex: 0,
+            },
+            {
+                hiragana: 'り',
+                katakana: 'リ',
+                en: 'ri',
+                index: 8,
+                subindex: 1,
+            },
+            {
+                hiragana: 'る',
+                katakana: 'ル',
+                en: 'ru',
+                index: 8,
+                subindex: 2,
+            },
+            {
+                hiragana: 'れ',
+                katakana: 'レ',
+                en: 're',
+                index: 8,
+                subindex: 3,
+            },
+            {
+                hiragana: 'ろ',
+                katakana: 'ロ',
+                en: 'ro',
+                index: 8,
+                subindex: 4,
+            },
+        ],
+        [{
+                hiragana: 'わ',
+                katakana: 'ワ',
+                en: 'wa',
+                index: 9,
+                subindex: 0,
+            },
+            {
+                hiragana: 'い',
+                katakana: 'イ',
+                en: 'i',
+                index: 9,
+                subindex: 1,
+            },
+            {
+                hiragana: 'う',
+                katakana: 'ウ',
+                en: 'u',
+                index: 9,
+                subindex: 2,
+            },
+            {
+                hiragana: 'え',
+                katakana: 'エ',
+                en: 'e',
+                index: 9,
+                subindex: 3,
+            },
+            {
+                hiragana: 'を',
+                katakana: 'ヲ',
+                en: 'wo',
+                index: 9,
+                subindex: 4,
+            },
+        ],
+        [{
+            hiragana: 'ん',
+            katakana: 'ン',
+            en: 'n',
+            index: 10,
+            subindex: 0,
+        }, ],
     ]
 }
 
@@ -108,13 +403,13 @@ function shuffle(array) {
 Page({
     data: {
         type: 2, //0平假名 1片假名 2都显示,
-        displayEn: 0,
-        finish:false,
+        displayEn: 1,
+        finish: false,
         jp: null,
         selected: [-1, -1],
     },
     onLoad: function(options) {
-        jp.items = shuffle(jp.items);
+        // jp.items = shuffle(jp.items);
         this.setData({
             jp
         });
@@ -149,20 +444,23 @@ Page({
         jp.items = shuffle(jp.items);
         this.setData({
             jp,
-            finish:false,
+            finish: false,
         });
     },
     check(items) {
         let isCorrect = true;
         for (let i = 0; i < items.length; i++) {
             for (let j = 0; j < items[i].length; j++) {
-                let {index, subindex} = items[i][j];
+                let {
+                    index,
+                    subindex
+                } = items[i][j];
                 if (!(index == i && subindex == j)) {
                     isCorrect = false;
                     break;
                 }
             }
-            if(!isCorrect) {
+            if (!isCorrect) {
                 break;
             }
         }
