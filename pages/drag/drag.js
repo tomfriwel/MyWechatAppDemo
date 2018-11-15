e7685e29250ff7e69280b4b8a55482f125145874
tomfriwel <976932447@qq.com>
2018-11-15 10:33:46 +0800

    add drag

---
+++ pages/drag/drag.js
@@ -0,0 +1,34 @@
+// pages/drag/drag.js
+
+Page({
+    data: {
+        items:[
+            {
+                x:100,
+                y:10
+            },
+            {
+                x:100,
+                y:140
+            },
+            {
+                x:200,
+                y:100
+            },
+            {
+                x: 230,
+                y: 400
+            },
+            {
+                x: 30,
+                y: 300
+            },
+        ]
+    },
+    onLoad: function(options) {
+
+    },
+    onReady: function() {
+
+    },
+})
