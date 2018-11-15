83905d1f911ff1d917765119b13443109c10e824
tomfriwel <976932447@qq.com>
2018-11-15 10:43:35 +0800

    endHandler

--- pages/drag/drag.js
+++ pages/drag/drag.js
@@ -2,18 +2,17 @@
 
 Page({
     data: {
-        items:[
-            {
-                x:100,
-                y:10
+        items: [{
+                x: 100,
+                y: 10
             },
             {
-                x:100,
-                y:140
+                x: 100,
+                y: 140
             },
             {
-                x:200,
-                y:100
+                x: 200,
+                y: 100
             },
             {
                 x: 230,
@@ -31,4 +30,28 @@ Page({
     onReady: function() {
 
     },
+    endHandler: function(e) {
+        let {
+            index
+        } = e.currentTarget.dataset
+        console.log(e)
+        let {
+            changedTouches
+        } = e
+        if (changedTouches.length == 1) {
+            let touch = changedTouches[0]
+            let {
+                pageX,
+                pageY
+            } = touch
+
+            this.data.items[index] = {
+                x: pageX-25,
+                y: pageY-25
+            }
+            this.setData({
+                items: this.data.items
+            })
+        }
+    }
 })
