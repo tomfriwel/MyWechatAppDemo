446408f49767dfec65ab4f99ad9f3f224d963bba
tomfriwel <976932447@qq.com>
2019-02-21 13:30:32 +0800

    clear

--- pages/test/test.js
+++ pages/test/test.js
@@ -6,44 +6,5 @@ Page({
         this.add()
     },
     add() {
-        let { tires} = this.data
-        tires.unshift({
-            positionIndex: null,
-            position: [
-                '前左',
-                '前右',
-                '后左',
-                '后右',
-            ],
-            brandnames: [],
-            brandnameIndex: null,
-            series: [],
-            seriesIndex: null,
-            specifications: [],
-            specificationsIndex: null,
-            figure: [],
-            figureIndex: null,
-            image: null,
-            tirebatchno: '',
-            specId: '',
-            time: (new Date()).getTime()
-        })
-        this.setData({
-            tires
-        })
-    },
-    positionPickerChange(e) {
-        let {
-            key,
-            index
-        } = e.currentTarget.dataset
-        let tires = this.data.tires
-
-        tires[index].positionIndex = +e.detail.value
-        tires[index].time = (new Date()).getTime()
-
-        console.log(tires[index])
-
-        this.setData({tires})
     },
 })
